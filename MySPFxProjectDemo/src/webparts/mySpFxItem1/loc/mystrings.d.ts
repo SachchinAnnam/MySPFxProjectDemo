8de16ebfc1bc32863882dfcc4e50c8665ed214e4
Sachchin Annam <sachchinannam@gmail.com>
2020-01-15 03:54:24 +0530

    push changes to develop (#1)

---
+++ MySPFxProjectDemo/src/webparts/mySpFxItem1/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IMySpFxItem1WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'MySpFxItem1WebPartStrings' {
+  const strings: IMySpFxItem1WebPartStrings;
+  export = strings;
+}
